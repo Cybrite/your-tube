b0fde9bffd08cfef3a0cfe38be26cf87feceb08f
Harsh <harshtanishq2002@gmail.com>
2024-09-11 16:32:34 +0530

    feat: added watchHistory controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@ import uploadOnCloudinary from "../utils/cloudinary.js";
 import ApiResponse from "../utils/apiResponse.js";
 import jwt from "jsonwebtoken";
 import deleteAfterChange from "../utils/deleteAfterChange.js";
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshTokens = async (userId) => {
     try {
@@ -397,6 +398,7 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
                 email: 1,
                 password: 0,
                 refreshToken: 0,
+                watchHistory: 0,
             },
         },
     ]);
@@ -416,6 +418,68 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
         );
 });
 
+const getWatchHistory = asyncHandler(async (req, res) => {
+    // req.user._id // gives a string
+
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(req.user._id),
+            },
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1,
+                                    },
+                                },
+                            ],
+                        },
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                // $arrayElemAt: ["$owner", 0]
+                                $first: "$owner",
+                            },
+                        },
+                    },
+                ],
+            },
+        },
+    ]);
+
+    if (!user?.length) {
+        throw new ApiError(404, "User not found");
+    }
+
+    return res
+        .status(200)
+        .json(
+            new ApiResponse(
+                200,
+                user[0].watchHistory,
+                "Watch History fetched successfully"
+            )
+        );
+});
+
+//exports
 export default registerUser;
 export {
     loginUser,
@@ -427,4 +491,5 @@ export {
     updateAvatar,
     updateCover,
     getUserChannelProfile,
+    getWatchHistory,
 };
