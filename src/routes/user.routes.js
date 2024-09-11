68ed0b4ccacede5c9175016f789c9749ee5168c3
Harsh <harshtanishq2002@gmail.com>
2024-09-11 16:50:31 +0530

    feat: added new routes for user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,15 @@
 import { Router } from "express";
 import registerUser, {
+    changeCurrentPassword,
+    getCurrentUser,
+    getUserChannelProfile,
+    getWatchHistory,
     loginUser,
     logoutUser,
     refreshAccessToken,
+    updateAccountDetails,
+    updateAvatar,
+    updateCover,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -28,5 +35,19 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-User").get(verifyJWT, getCurrentUser);
+router.route("/update-account-details").patch(verifyJWT, updateAccountDetails);
+
+router
+    .route("/update-Avatar")
+    .patch(verifyJWT, upload.single("avatar"), updateAvatar);
+router
+    .route("/update-Cover")
+    .patch(verifyJWT, upload.single("coverImage"), updateCover);
+
+//to get user profile
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile); //when you are using params
+router.route("/watchHistory").get(verifyJWT, getWatchHistory);
 
 export default router;
